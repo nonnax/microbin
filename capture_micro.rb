54bcfba36e5dd0e6b9453d3e044d91e7fd2804da
nonnax <ironald@gmail.com>
2021-12-16 01:23:37 +0800

    update: capture_micro.rb

--- capture_micro.rb
+++ capture_micro.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2021-12-15 22:02:15 +0800
 buff=$stdin.read
-IO.popen("wput", 'w+'){|io| io.puts buff; io.close_write; io.read}
+IO.popen("wput", 'w+'){|io| io.puts buff; io.close_write}
